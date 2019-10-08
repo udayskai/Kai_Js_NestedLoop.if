3bafc27911587ed412cdd3442cb15148dd8edf02
uday singh <[udayskai@gmail.com]>
2019-10-08 23:34:59 +0530

    Work penidng

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+let Mark=122;
+if((Mark>=0)
+&&
+   (Mark<=100)) 
+{
+    if( (mark>=0)&&(mark<=35));
+    {
+        console.log("Fail");
+    }
+    
+     if else((mark>35)&&(mark<=50))
+    {
+        console.log("Secand class");
+    }
+}
+
+else
+{
+    console.log("Enter number between 0 & 100")
+}
