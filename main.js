af15341beb91d241f6df312e8b65b00438f78411
uday singh <[udayskai@gmail.com]>
2019-10-10 11:49:58 +0530

    Capital letter issue solved

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 "use strict";
 
 
-let Mark=122;
-if((Mark>=0) && (Mark<=100)) 
-{
-    if else( (mark>=0&&mark<=35));
-    {
-        console.log("Fail");
+let marks=34;
+let message=undefined;
+
+if((marks>=0)
+     &&
+   (marks<=100)
+   ) 
+
+   {
+    if (marks >= 35 && marks < 49) {
+        message = "Pass ";
     }
-    
-     if else((mark>35)&&(mark<=50))
-    {
-        console.log("Secand class");
+    else if (marks >= 50 && marks < 60) {
+        message = "Second Class";
     }
-}
+    else if (marks >= 60 && marks < 75) {
+        message = "First Class";
+    }
+    else if (marks >= 75 && marks <= 100) {
+        message = "Distinction";
+    }
+    else{
+          message='fail';
+    }
+
+   }
 
 else
-{
-    console.log("Enter number between 0 & 100")
-}
+      ("Enter number between 0 & 100");
+       {  console.log(message); 
+     }
+
+
+
+
+
+
+
